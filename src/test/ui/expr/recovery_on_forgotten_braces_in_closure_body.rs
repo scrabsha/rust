31b526b5156140592079602514cf5dbf54c47184
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-08-17 13:50:37 +0200

    Add link to gh issue

--- src/test/ui/expr/recovery_on_forgotten_braces_in_closure_body.rs
+++ src/test/ui/expr/recovery_on_forgotten_braces_in_closure_body.rs
@@ -1,6 +1,9 @@
 // This test corresponds to issue #88065 which points out that there is no
 // recovery when parsing a block closure body when there is no braces around
 // its statements.
+//
+// Link to the original issue:
+// https://github.com/rust-lang/rust/issues/88065
 
 fn main() {
     let num = 5;
