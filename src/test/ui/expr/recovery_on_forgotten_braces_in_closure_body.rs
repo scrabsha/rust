68eb8773617445bb957c393d176fd92703b611bc
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-08-17 13:47:59 +0200

    Fix error message in test

--- src/test/ui/expr/recovery_on_forgotten_braces_in_closure_body.rs
+++ src/test/ui/expr/recovery_on_forgotten_braces_in_closure_body.rs
@@ -1,7 +1,11 @@
+// This test corresponds to issue #88065 which points out that there is no
+// recovery when parsing a block closure body when there is no braces around
+// its statements.
+
 fn main() {
     let num = 5;
     (1..num).reduce(|a, b| 
-        println!("{}", a);
+        println!("{}", a); //~ ERROR: Missing braces around block
         a * b
     ).unwrap();
 }
